afd08d8b507b0d3324b9c55d045ee1d22d8c0bbb
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2023-11-15 18:27:45 +0300

    feature: added handling of missing exceptions
    * Edited ControllerAdvice.java

--- src/main/java/com/mag/taskList/web/controller/ControllerAdvice.java
+++ src/main/java/com/mag/taskList/web/controller/ControllerAdvice.java
@@ -1,12 +1,20 @@
 package com.mag.taskList.web.controller;
 
+import com.mag.taskList.domain.exception.AccessDeniedException;
 import com.mag.taskList.domain.exception.ExceptionBody;
 import com.mag.taskList.domain.exception.ResourceMappingException;
 import com.mag.taskList.domain.exception.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Обработка исключений в контроллерах
@@ -30,5 +38,72 @@ public class ControllerAdvice {
     public ExceptionBody handleResourceMapping(ResourceMappingException e) {
         return new ExceptionBody(e.getMessage());
     }
+
+    /**
+     * Проверка на плохой запрос
+     * @param e сообщение, которое необходимо отобразить
+     * @return объект класса {@link ExceptionBody}
+     */
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleIllegalState(IllegalStateException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, org.springframework.security.access.AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionBody handleAccessDenied() {
+        return new ExceptionBody("Access denied.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        // Создать объект
+        ExceptionBody exceptionBody = new ExceptionBody("Validation failed.");
+        // Список ошибок, связанных с полями
+        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        exceptionBody.setErrors(errors.stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
+
+        return exceptionBody;
+    }
+
+    /**
+     * Проверка на заполнение всех полей
+     * @param e ошибка, которую необходимо отобразить
+     * @return класс объекта {@link ExceptionBody}
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleConstraintViolation(ConstraintViolationException e) {
+        // Создать объект
+        ExceptionBody exceptionBody = new ExceptionBody("Validation failed.");
+        // Добавить ошибки
+        exceptionBody.setErrors(e.getConstraintViolations().stream()
+                .collect(Collectors.toMap(
+                        violation -> violation.getPropertyPath().toString(),
+                        violation -> violation.getMessage()
+                )));
+        return exceptionBody;
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleAuthentication(AuthenticationException e) {
+        return new ExceptionBody("Authentication failed");
+    }
+
+    /**
+     * Метод, который проверяет оставшиеся исключения
+     * @param e ошибка, которую необходимо отобразить
+     * @return класс объекта {@link ExceptionBody}
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionBody handleException(Exception e) {
+        return new ExceptionBody("Internal error.");
+    }
+
 
 }
